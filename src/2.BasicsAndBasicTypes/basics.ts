ce1bb9560cae789dfca02bd98e89fc2fb19526de
i.kłusek <J6ZkqwVUn>
2022-09-23 21:27:19 +0200

    Compiler settings

---
+++ src/2.BasicsAndBasicTypes/basics.ts
@@ -0,0 +1,14 @@
+function add1(n1: number, n2: number, showResult: boolean, phrase: string) {
+    if (showResult) {
+        console.log(`${phrase}${n1 + n2}`);
+    } else {
+        return n1 + n2;
+    }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult1 = true;
+let resultPhrase = 'Result is ';
+
+console.log(add1(number1, number2, printResult1, resultPhrase));
